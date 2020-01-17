955eb89f1ddd86e3dd73ee7c1510a6bd2b60c00d
wymerkd <wymerkd@gmail.com>
2020-01-17 14:52:12 -0800

    add callback for product name and country of origin

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates_length_of :name, maximum: 40
+  before_save(:titleize_product)
+
+ private
+   def titleize_product
+     self.name = self.name.titleize
+     self.country = self.country.titleize
+   end
 end
