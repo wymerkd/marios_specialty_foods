4e9b2d434d36bee69902c0a337f3c31974f6246a
wymerkd <wymerkd@gmail.com>
2020-01-17 14:40:52 -0800

    add validation spec for maximum char

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
+  validates_length_of :name, maximum: 40
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Product do
   it { should have_many(:reviews) }
   it { should validate_presence_of :name }
+  it { should validate_length_of(:name).is_at_most(40) }
 end
