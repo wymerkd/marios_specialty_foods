0a1fa79dbb22e3af0cff279450ec387a4a4c4e69
wymerkd <wymerkd@gmail.com>
2020-01-17 17:51:18 -0800

    validate and write test to set limit for max rating set to 5.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,5 +5,6 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content }
   it { should validate_presence_of :rating }
+  it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
   it { should validate_length_of(:author).is_at_most(40) }
 end
