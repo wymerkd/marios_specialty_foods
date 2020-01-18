afbe0e3528f5bb64507bc9f1903ab90a9a0587c0
wymerkd <wymerkd@gmail.com>
2020-01-17 17:55:28 -0800

    validate and test for content min/max length.

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,6 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates :rating, numericality: { less_than_or_equal_to: 5,  only_integer: true }
   validates_length_of :author, maximum: 40
+  validates_length_of :content, minimum: 50
+  validates_length_of :content, maximum: 250
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,4 +7,6 @@ describe Review do
   it { should validate_presence_of :rating }
   it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
   it { should validate_length_of(:author).is_at_most(40) }
+  it { should validate_length_of(:content).is_at_least(50) }
+  it { should validate_length_of(:content).is_at_most(250) }
 end
