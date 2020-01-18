2764958033e10c150338d5c1ea2ba15190f310d7
wymerkd <wymerkd@gmail.com>
2020-01-17 17:29:09 -0800

    add validations for content and rating presence.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true
   validates_length_of :author, maximum: 40
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 describe Review do
   it { should belong_to(:product) }
   it { should validate_presence_of :author }
+  it { should validate_presence_of :content }
+  it { should validate_presence_of :rating }
   it { should validate_length_of(:author).is_at_most(40) }
 end
