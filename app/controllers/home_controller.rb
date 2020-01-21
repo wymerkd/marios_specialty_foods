74e656f95ac90017174c59a16c57815fcdaad85f
wymerkd <wymerkd@gmail.com>
2020-01-20 20:43:12 -0800

    add scope for most reviewed products and add link to homepage on products page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-    @products = Product.all
+    @products_most_reviews = Product.most_reviews
+    @products_most_recent = Product.three_most_recent
     render :index
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ApplicationRecord
   scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :most_reviews, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(10)
+    )}
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates_length_of :name, maximum: 40
